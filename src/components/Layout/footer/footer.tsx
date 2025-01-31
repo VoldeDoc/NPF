ad596c5fee2db15ce37811d5b91c478c0473a752
Emmanuella56 <emmanuella.uduji@miva.edu.ng>
2025-01-31 05:16:32 -0800

    adjustment made on header and footer

--- src/components/Layout/footer/footer.tsx
+++ src/components/Layout/footer/footer.tsx
@@ -2,82 +2,111 @@ import { BiLogoFacebook} from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { SiInstagram } from "react-icons/si";
 import { TbBrandX} from "react-icons/tb";
+import Logo from "../../../assets/Logo.png"
+
 export default function Footer() {
   return (
     <>
-      <footer className="text-white bg-green-800 pt-20 ">
+      <footer className="text-white bg-green-800 py-10 ">
         <div className="px-8 sm:px-16">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-            <div>
-            <img src="/public/assets/logo/footer.png" alt="" />
-
-              <h1 className="text-5xl font-bold text-white">Subscribe for our </h1>
-              <h1 className="text-5xl font-bold text-white">newsletter</h1>
-              <div className="mt-5 flex items-center max-w-md">
-                <input
-                  type="email"
-                  placeholder="E-mail"
-                  className="flex-grow px-4 py-2 border border-white bg-green-800 focus:outline-none rounded-lg w-96"
-                />
-                <button className="px-4 py-2 bg-green-800 text-white rounded-r-md flex items-center justify-center">
-                  →
-                </button>
-              </div>
-
-            </div>
-
-            <div>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
+            
               <div>
-                <h1 className="text-gray-100 pb-1 text-xs"> ABOUT US</h1>
-                <div className="py-2">
-                  <ul className="text-lg">
-                    <li><a href="">Privacy</a></li>
-                    <li><a href="">Claims</a></li>
-                    <li><a href="">Third Party claims</a></li>
-                    <li><a href="">About Us</a></li>
-                  </ul>
+                <div className="text-2xl font-bold md:w-[300px]">
+                  <img src={Logo} alt="NPF Logo"  />
                 </div>
+                
+
+                  <h1 className=" text-4xl md:text-5xl font-bold pt-[-10px] text-white mt-6">Subscribe for our </h1>
+                  <h1 className="text-4xl md:text-5xl font-bold text-white">newsletter</h1>
+                  <div className="mt-5 flex items-center max-w-md">
+                    <input
+                      type="email"
+                      placeholder="E-mail"
+                      className="flex-grow px-4 py-2 border border-white bg-green-800 focus:outline-none rounded-lg w-96"
+                    />
+                    <button className="px-4 py-2 bg-green-800 text-white rounded-r-md flex items-center justify-center">
+                      →
+                    </button>
+                  </div>
+
+
+                
               </div>
 
-              <div className="py-7">
-                <h1 className="text-gray-100 pb-1 text-xs">CONTACT US</h1>
-                <div className="py-2">
-                  <ul className="text-lg">
-                    <li>
-                      <a href="tel:+2349054110010">+234-905-411-0010</a>
-                    </li>
-                    <li>
-                      <a href="tel:+2349054110011">+234-905-411-0011</a>
-                    </li>
-                    <li>
-                      <a href="mailto:contact@npfinsurance.com">contact@npfinsurance.com</a>
-                  </li>
-                  </ul>
+              <div>
+                  <div>
+                    <h1 className="text-gray-100 pb-1 text-xs"> ABOUT US</h1>
+                    <div className="py-2">
+                      <ul className="text-lg">
+                        <li><a href="">Privacy</a></li>
+                        <li><a href="">Claims</a></li>
+                        <li><a href="">Third Party claims</a></li>
+                        <li><a href="">About Us</a></li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className="pt-7">
+                    <h1 className="text-gray-100 pb-1 text-xs">CONTACT US</h1>
+                    <div className="py-2">
+                      <ul className="text-lg">
+                        <li>
+                          <a href="tel:+2349054110010">+234-905-411-0010</a>
+                        </li>
+                        <li>
+                          <a href="tel:+2349054110011">+234-905-411-0011</a>
+                        </li>
+                        <li>
+                          <a href="mailto:contact@npfinsurance.com">contact@npfinsurance.com</a>
+                      </li>
+                      </ul>
+                    </div>
                 </div>
+
               </div>
 
-            </div>
+              <div>
+                        <div>
+                          <h1 className="text-gray-100 pb-1 text-xs"> ADDRESS</h1>
+                          <div className="py-2">
+                              <ul className="text-lg">
+                                <li>Insurance Building,</li>
+                                <li>Behind Louis Edet House</li>
+                                <li>Force Headquarters, Shehu Shagari Way, Abuja </li>
+                                <li> Abuja </li>
+                              </ul>
+                          </div>
+                        </div>
+              </div>
 
+              <div className="grid grid-cols-1 md:grid-cols-2">
+                
+                <div className="flex space-x-9">
+                <Link to={"https://www.facebook.com/profile.php?id=61569490243943"}>
+                  <BiLogoFacebook className="text-white text-lg"/>
+                </Link>
+                <Link to={"https://www.instagram.com/npfinsurance?igsh=MXV4a280dDBvNzNw"}>
+                  <SiInstagram className="text-white text-lg"/>
+                </Link>
+                <Link to={"https://x.com/npfInsurance?t=fr1lvo6fvgRCO37JhzPdHw&s=09"}>
+                  <TbBrandX className="text-white text-lg"/>
+                </Link>
+              </div>
 
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 py-20">
-            <div className="flex space-x-9">
-            <Link to={"https://www.facebook.com/profile.php?id=61569490243943"}>
-              <BiLogoFacebook className="text-white text-lg"/>
-            </Link>
-            <Link to={"https://www.instagram.com/npfinsurance?igsh=MXV4a280dDBvNzNw"}>
-              <SiInstagram className="text-white text-lg"/>
-            </Link>
-            <Link to={"https://x.com/npfInsurance?t=fr1lvo6fvgRCO37JhzPdHw&s=09"}>
-              <TbBrandX className="text-white text-lg"/>
-            </Link>
+              
             </div>
-            <div>
-              <h1  className="text-xs text-gray-200 pb-4">Pricacy</h1>
-              <h1>© {new Date().getFullYear()}-copyright</h1>
+       
+            <div className="text-sm">
+                <p > &#x00A9; {new Date().getFullYear()} - NPF Insurance Company LTD.</p>
+                <p> All rights reserved.</p>
             </div>
-          </div>
+            
+       
         </div>
+
+       </div> 
       </footer>
     </>
   );
--- src/components/Layout/header/header.tsx
+++ src/components/Layout/header/header.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { MdMenu, MdClose } from 'react-icons/md';
 import NavLinks from './Tools/NavLink';
 import { Link } from 'react-router-dom';
-
+import Logo from "../../../assets/Logo.png"
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -12,9 +12,9 @@ export default function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-0 w-full bg-white text-black-500 py-4 px-16 flex justify-between items-center z-10">
-        <div className="text-2xl font-bold">
-          <img src="assets/logo/NPF logo 5.svg" alt="" />
+      <header className="fixed top-0 left-0 w-full bg-white text-black-500 px-16 py-2 flex justify-between items-center z-10">
+        <div className="text-2xl font-bold ">
+          <img src={Logo} alt="logo" className=' h-[40px]  w-[200px] md:w-[300px] md:h-[50px]' />
         </div>
         <div className="hidden md:flex justify-center flex-1 px-10">
           <NavLinks />
@@ -31,17 +31,18 @@ export default function Header() {
         </button>
       </header>
       <aside className={`fixed top-0 left-0 w-64 h-full bg-white text-black-500 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:hidden z-20`}>
+        
         <div className="p-4">
           <div className="text-2xl font-bold mb-4">
             <img src="assets/logo/NPF logo 5.svg" alt="" />
           </div>
-          <nav className="flex flex-col space-y-4">
+          <nav className="flex flex-col space-y-2">
             <NavLinks />
           </nav>
         </div>
-        <div className="absolute bottom-4 left-0 w-full px-4">
+        <div className="absolute bottom-2 left-0 w-full px-4">
           <Link to={'/insurance-quote'}>
-            <button className="bg-green-900 text-white w-full px-4 py-2 rounded">
+            <button className="bg-green-900 text-white w-full px-2 rounded">
               Get a Quote
             </button>
           </Link>
